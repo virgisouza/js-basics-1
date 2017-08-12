723d8be3643c93ece5bceb0af37e948ab2b6bc7e
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-08-12 12:51:18 -1000

    third commit

--- basics.js
+++ basics.js
@@ -16,6 +16,8 @@ var myName = "Marge Simpson";
  * if it should be true if your person object is at least 16 years old
  */
 
+ var canDrive = true;
+
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
